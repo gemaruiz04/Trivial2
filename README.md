3004238e389759196737f9cec4234480f219b5ae
Gema Ruiz Martín <gema.ruizmartin@iesfm.onmicrosoft.com>
2022-02-18 09:33:42 +0100

    Eliminamos código repetido de posición de jugador

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Trivial2
+
+## Problema 0
+
+Extraemos el codigo duplicado a un único método "nuevaPosicionJugador",
+al que llamamos desde ambos sitios
+
+Creamos test unitario "si_al_principio_saco_un_1_voy_a_casilla_1"
--- test/TrivialTests.java
+++ test/TrivialTests.java
@@ -12,4 +12,15 @@ public class TrivialTests {
     public void crear_Game(){
         Game trivial = new Game();
     }
+
+    @Test
+    public void si_al_principio_saco_un_1_voy_a_casilla_1(){
+        Game jugador = new Game();
+        jugador.agregar("Maria");
+        jugador.agregar("Juan");
+        jugador.tirarDado(1);
+        String expected = "La nueva posición de Maria es 1";
+        String actual = jugador.nuevaPosicionJugador();
+        Assertions.assertEquals(expected,actual);
+    }
 }
